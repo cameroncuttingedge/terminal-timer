c7bdc9b1e834a3a009142d1b864f78095b6bc260
Cameron <109548666+cameroncuttingedge@users.noreply.github.com>
2024-02-16 13:03:18 -0800

    Revert "Db"

--- display/display.go
+++ display/display.go
@@ -2,8 +2,6 @@ package display
 
 import (
 	"fmt"
-	"math"
-	"os"
 	"strings"
 	"terminal-timer/art"
 	"terminal-timer/util"
@@ -26,73 +24,76 @@ func NewDisplayMatrix(width, height int) *DisplayMatrix {
 	return &DisplayMatrix{Width: width, Height: height, Matrix: matrix}
 }
 
+
 func (dm *DisplayMatrix) AddCenteredMessage(message string) {
-	lines := strings.Split(message, "\n")
-	totalLines := len(lines)
+    lines := strings.Split(message, "\n")
+    totalLines := len(lines)
 
-	startY := (dm.Height - totalLines) / 2
-	if startY < 0 {
-		startY = 0
-	}
+    startY := (dm.Height - totalLines) / 2
+    if startY < 0 {
+        startY = 0
+    }
 
-	for i, line := range lines {
-		maxWidth := len(line)
-		startX := (dm.Width - maxWidth) / 2
-		if startX < 0 {
-			startX = 0
-		}
+    for i, line := range lines {
+        maxWidth := len(line)
+        startX := (dm.Width - maxWidth) / 2
+        if startX < 0 {
+            startX = 0
+        }
 
-		for x, char := range line {
-			matrixY := startY + i
-			matrixX := startX + x
-			if matrixY < dm.Height && matrixX < dm.Width {
-				dm.Matrix[matrixY][matrixX] = char
-			}
-		}
-	}
+        for x, char := range line {
+            matrixY := startY + i
+            matrixX := startX + x
+            if matrixY < dm.Height && matrixX < dm.Width {
+                dm.Matrix[matrixY][matrixX] = char
+            }
+        }
+    }
 }
 
+
 func (dm *DisplayMatrix) AddCenteredAsciiArt(asciiArt []string, message string) {
-	artHeight := len(asciiArt)
-	artWidth := 0
-	for _, line := range asciiArt {
-		if len(line) > artWidth {
-			artWidth = len(line)
-		}
-	}
+    artHeight := len(asciiArt)
+    artWidth := 0
+    for _, line := range asciiArt {
+        if len(line) > artWidth {
+            artWidth = len(line)
+        }
+    }
 
-	// Check if the art will fit vertically and horizontally
-	if artHeight > dm.Height || artWidth > dm.Width {
-		dm.AddCenteredMessage(message)
-		return
-	}
+    // Check if the art will fit vertically and horizontally
+    if artHeight > dm.Height || artWidth > dm.Width {
+        dm.AddCenteredMessage(message)
+        return
+    }
 
-	startY := (dm.Height - artHeight) / 2
-	startX := (dm.Width - artWidth) / 2
+    startY := (dm.Height - artHeight) / 2
+    startX := (dm.Width - artWidth) / 2
 
-	for y, line := range asciiArt {
-		for x, char := range line {
-			matrixY := startY + y
-			matrixX := startX + x
-			if matrixY < dm.Height && matrixX < dm.Width {
-				dm.Matrix[matrixY][matrixX] = char
-			}
-		}
-	}
+    for y, line := range asciiArt {
+        for x, char := range line {
+            matrixY := startY + y
+            matrixX := startX + x
+            if matrixY < dm.Height && matrixX < dm.Width {
+                dm.Matrix[matrixY][matrixX] = char
+            }
+        }
+    }
 }
 
 func (dm *DisplayMatrix) Print() {
-	var output strings.Builder
-	for _, row := range dm.Matrix {
-		for _, char := range row {
-			output.WriteRune(char)
-		}
-		output.WriteRune('\n')
-	}
-	util.HideCursor()
-	util.Clear()
-	fmt.Print(output.String())
+    var output strings.Builder
+    for _, row := range dm.Matrix {
+        for _, char := range row {
+            output.WriteRune(char)
+        }
+        output.WriteRune('\n')
+    }
+    util.HideCursor()
+    util.Clear()
+    fmt.Print(output.String())
 }
+
 
 func (dm *DisplayMatrix) ResizeAndClear() {
 	width, height, err := util.GetSize()
@@ -100,119 +101,48 @@ func (dm *DisplayMatrix) ResizeAndClear() {
 		fmt.Println("Error getting terminal size:", err)
 		return
 	}
-	newMatrix := make([][]rune, height)
-	for i := range newMatrix {
-		newMatrix[i] = make([]rune, width)
-		for j := range newMatrix[i] {
-			newMatrix[i][j] = ' '
-		}
-	}
-	dm.Width = width
-	dm.Height = height
-	dm.Matrix = newMatrix
+    newMatrix := make([][]rune, height)
+    for i := range newMatrix {
+        newMatrix[i] = make([]rune, width)
+        for j := range newMatrix[i] {
+            newMatrix[i][j] = ' ' 
+        }
+    }
+    dm.Width = width
+    dm.Height = height
+    dm.Matrix = newMatrix
 }
+
 
 func (dm *DisplayMatrix) AddBottomLeftMessage(message string) {
-	lines := strings.Split(message, "\n")
-	totalLines := len(lines)
+    lines := strings.Split(message, "\n")
+    totalLines := len(lines)
 
-	startY := dm.Height - totalLines
-	startX := 0
+    startY := dm.Height - totalLines
+    startX := 0 
 
-	for i, line := range lines {
-		for x, char := range line {
-			matrixY := startY + i
-			matrixX := startX + x
-			if matrixY >= 0 && matrixY < dm.Height && matrixX >= 0 && matrixX < dm.Width {
-				dm.Matrix[matrixY][matrixX] = char
-			}
-			if matrixX >= dm.Width-1 {
-				break
-			}
-		}
-		if startY+i >= dm.Height-1 {
-			break
-		}
-	}
+    for i, line := range lines {
+        for x, char := range line {
+            matrixY := startY + i
+            matrixX := startX + x
+            if matrixY >= 0 && matrixY < dm.Height && matrixX >= 0 && matrixX < dm.Width {
+                dm.Matrix[matrixY][matrixX] = char
+            }
+            if matrixX >= dm.Width - 1 {
+                break
+            }
+        }
+        if startY+i >= dm.Height-1 {
+            break
+        }
+    }
 }
+
 
 func BufferEndMessage(matrix *DisplayMatrix, reminder string, font string) {
-	matrix.ResizeAndClear()
-	timeUpMessage := art.GetAsciiArt(reminder, font)
-	matrix.AddCenteredAsciiArt(timeUpMessage, reminder)
-	message := "Press 'q' to quit or 'r' to repeat."
-	matrix.AddBottomLeftMessage(message)
-}
-
-func (dm *DisplayMatrix) PrintItemsInGrid(items []string, columns int) {
-	// Calculate necessary dimensions
-	rows := int(math.Ceil(float64(len(items)) / float64(columns)))
-	maxItemLength := 0
-	for _, item := range items {
-		if len(item) > maxItemLength {
-			maxItemLength = len(item)
-		}
-	}
-
-	// Calculate starting positions
-	totalWidth := maxItemLength*columns + (columns-1)*2 // Assuming 2 spaces as padding between columns
-	startX := (dm.Width - totalWidth) / 2
-	startY := (dm.Height - rows) / 2
-
-	// Iterate over items and print them in the specified grid layout
-	for i, item := range items {
-		row := i / columns
-		col := i % columns
-
-		posX := startX + col*(maxItemLength+2) // +2 for padding between columns
-		posY := startY + row
-
-		if posY >= 0 && posY < dm.Height && posX >= 0 && posX+maxItemLength <= dm.Width {
-			copy(dm.Matrix[posY][posX:posX+maxItemLength], []rune(fmt.Sprintf("%-*s", maxItemLength, item)))
-		}
-	}
-}
-
-func RenderPrettyData(fonts []string) {
-	width, height, err := util.GetSize()
-	if err != nil {
-		fmt.Printf("Error getting terminal size: %v\n", err)
-		return
-	}
-	matrix := NewDisplayMatrix(width, height)
-
-	// Determine the number of columns based on the terminal width
-	maxLen := 0
-	for _, font := range fonts {
-		if len(font) > maxLen {
-			maxLen = len(font)
-		}
-	}
-	columns := int(math.Floor(float64(width) / float64(maxLen+2))) // 2 spaces padding
-
-	// Use the DisplayMatrix to print the fonts in a grid
-	matrix.PrintItemsInGrid(fonts, columns)
-	matrix.Print()
-
-	os.Exit(1)
-}
-
-func RenderFontExample(font string) {
-
-	message := "420:69"
-
-	width, height, err := util.GetSize()
-	if err != nil {
-		fmt.Printf("Error getting terminal size: %v\n", err)
-		return
-	}
-	matrix := NewDisplayMatrix(width, height)
-
-	art := art.GetAsciiArt(message, font)
-
-	matrix.AddCenteredAsciiArt(art, message)
-
-	matrix.Print()
-
-	os.Exit(1)
+    matrix.ResizeAndClear()
+    timeUpMessage := art.GetAsciiArt(reminder, font)
+    matrix.AddCenteredAsciiArt(timeUpMessage, reminder)
+    message := "Press 'q' to quit or 'r' to repeat."
+    matrix.AddBottomLeftMessage(message)
 }
--- main.go
+++ main.go
@@ -9,11 +9,10 @@ import (
 	"syscall"
 	"time"
 
-	"terminal-timer/alert"
 	"terminal-timer/art"
-	"terminal-timer/config"
 	"terminal-timer/display"
 	"terminal-timer/random"
+	"terminal-timer/sound"
 	"terminal-timer/util"
 
 	"github.com/mattn/go-tty"
@@ -23,140 +22,137 @@ var userDecision = make(chan string, 1)
 
 // main initializes the application, parses flags, and starts the timer loop.
 func main() {
+	timerFlag, alarmFlag, reminderFlag, enableLogging := util.ParseFlags()
+    
+    random.GenerateTempFileName()
 
-	util.ParseFlags()
+    if enableLogging {
+        util.SetupLogger()
+    }
 
-	fmt.Printf(*util.PreviewSoundFlag)
+	setupSignalHandling(cleanup)
 
-	config.LoadOrCreateConfig()
-
-	random.GenerateTempSoundFileName()
-
-	config.CheckIfconfigChangesRequested()
-
-	if *util.EnableLogging {
-		util.SetupLogger()
-	}
-
-	setupSignalHandling(util.Cleanup)
-
-	var directInput string
+    var directInput string
 	if len(flag.Args()) > 0 {
 		directInput = flag.Arg(0)
 	}
 
-	totalSeconds, err := util.CalculateTotalSeconds(*util.TimerFlag, *util.AlarmFlag, directInput)
-	if err != nil {
-		fmt.Println("Error parsing timer or alarm flag:", err)
-		return
-	}
+	totalSeconds, err := util.CalculateTotalSeconds(timerFlag, alarmFlag, directInput)
+    if err != nil {
+        fmt.Println("Error parsing timer or alarm flag:", err)
+        return
+    }
 
-	reminder := util.GetReminderMessage(*util.ReminderFlag)
+	reminder := util.GetReminderMessage(reminderFlag)
 	runTimerLoop(totalSeconds, reminder)
-	defer util.Cleanup()
+    defer cleanup()
 }
 
 // runTimerLoop runs the main timer loop, displaying time and handling user input.
 func runTimerLoop(totalSeconds int, reminder string) {
-	title := "Timer Completed"
-	soundPath := config.Sound
-	for {
-		util.HideCursor()
-		util.Render()
+    title := "Timer Completed"
+    soundPath := "Jinja.wav"
+    for {
+        util.HideCursor()
+        util.Render()
 
-		width, height, err := util.GetSize()
-		if err != nil {
-			fmt.Println("Error getting terminal size:", err)
-			return
-		}
+        width, height, err := util.GetSize()
+        if err != nil {
+            fmt.Println("Error getting terminal size:", err)
+            return
+        }
 
-		matrix := display.NewDisplayMatrix(width, height)
-		startTimer(totalSeconds, matrix)
-		display.BufferEndMessage(matrix, reminder, config.Font)
+        matrix := display.NewDisplayMatrix(width, height)
+        startTimer(totalSeconds, "", matrix)
+        display.BufferEndMessage(matrix, reminder, "")
 
-		matrix.Print()
-		alert.EndOfTimer(soundPath, title, reminder)
+        matrix.Print()
+        sound.EndOfTimer(soundPath, title, reminder)
 
-		if !waitForUserInput(matrix, reminder, "") {
-			break
-		}
-	}
+        if !waitForUserInput(matrix, reminder, "") {
+            break
+        }
+    }
 }
 
 // waitForUserInput waits for user input to restart or quit the timer.
 func waitForUserInput(matrix *display.DisplayMatrix, reminder, font string) bool {
-	tty, err := tty.Open()
-	if err != nil {
-		log.Fatalf("failed to open tty: %v", err)
-	}
-	defer tty.Close()
+    tty, err := tty.Open()
+    if err != nil {
+        log.Fatalf("failed to open tty: %v", err)
+    }
+    defer tty.Close()
 
-	go func() {
-		for {
-			r, err := tty.ReadRune()
-			if err != nil {
-				fmt.Printf("Error reading rune: %v", err)
-				continue
-			}
-			switch r {
-			case 'q', 'r':
-				userDecision <- string(r)
-				return
-			}
-		}
-	}()
+    go func() {
+        for {
+            r, err := tty.ReadRune()
+            if err != nil {
+                fmt.Printf("Error reading rune: %v", err)
+                continue
+            }
+            switch r {
+            case 'q', 'r':
+                userDecision <- string(r)
+                return
+            }
+        }
+    }()
 
-	for {
-		select {
-		case decision := <-userDecision:
-			util.ShowCursor()
-			return decision == "r"
-		default:
-			matrix.ResizeAndClear()
-			display.BufferEndMessage(matrix, reminder, config.Font)
-			matrix.Print()
-			time.Sleep(100 * time.Millisecond)
-		}
-	}
+    for {
+        select {
+        case decision := <-userDecision:
+            util.ShowCursor()
+            return decision == "r"
+        default:
+            matrix.ResizeAndClear()
+            display.BufferEndMessage(matrix, reminder, font)
+            matrix.Print()
+            time.Sleep(100 * time.Millisecond)
+        }
+    }
 }
 
 // startTimer counts down the timer and updates the display.
-func startTimer(totalSeconds int, matrix *display.DisplayMatrix) {
-	endTime := time.Now().Add(time.Duration(totalSeconds) * time.Second)
-
-	// Call the update function once before entering the loop to display the first tick
-	updateTimerDisplay(endTime, matrix)
-
-	ticker := time.NewTicker(time.Second)
-	defer ticker.Stop()
-
-	for range ticker.C {
-		if time.Now().After(endTime) {
+func startTimer(totalSeconds int, font string, matrix *display.DisplayMatrix) {
+    endTime := time.Now().Add(time.Duration(totalSeconds) * time.Second)
+	for range time.Tick(time.Second) {
+		remaining := time.Until(endTime)
+		if remaining <= 0 {
 			break
 		}
-		updateTimerDisplay(endTime, matrix)
-	}
-}
 
-// updateTimerDisplay handles updating and printing the timer to the display matrix
-func updateTimerDisplay(endTime time.Time, matrix *display.DisplayMatrix) {
-	remaining := time.Until(endTime)
-	timerRemaining := fmt.Sprintf("%02d:%02d:%02d", int(remaining.Hours()), int(remaining.Minutes())%60, int(remaining.Seconds())%60)
-	asciiArt := art.GetAsciiArt(timerRemaining, config.Font)
-	matrix.AddCenteredAsciiArt(asciiArt, timerRemaining)
-	matrix.Print()
-	matrix.ResizeAndClear()
+		message := fmt.Sprintf("%02d:%02d:%02d", int(remaining.Hours()), int(remaining.Minutes())%60, int(remaining.Seconds())%60)
+		asciiArt := art.GetAsciiArt(message, font)
+		matrix.AddCenteredAsciiArt(asciiArt, message)
+		matrix.Print()
+		matrix.ResizeAndClear()
+	}
 }
 
 // setupSignalHandling configures handling for SIGINT and SIGTERM.
 func setupSignalHandling(cleanupFunc func()) {
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+    c := make(chan os.Signal, 1)
+    signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+    
+    go func() {
+        <-c 
+        cleanupFunc()
+        fmt.Println("\nReceived Ctrl+C, exiting...")
+        os.Exit(0)
+    }()
+}
 
-	go func() {
-		<-c
-		cleanupFunc()
-		fmt.Println("\nReceived Ctrl+C, exiting...")
-		os.Exit(0)
-	}()
+// cleanup performs application cleanup tasks.
+func cleanup() {
+    util.ShowCursor()
+	util.Clear()
+    util.Render()
+    if random.TempFileName != "" {
+        err := os.Remove(random.TempFileName)
+        if err != nil {
+            log.Printf("Failed to delete temporary file %s: %v\n", random.TempFileName, err)
+        } else {
+            log.Printf("Temporary file %s deleted successfully\n", random.TempFileName)
+        }
+    }
 }
